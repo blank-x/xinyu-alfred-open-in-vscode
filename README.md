303c143b5ba39c25d11291c9b68318667b3559c8
zhaoshaoyong <zhaoshaoyong@360.cn>
2023-08-20 20:18:02 +0800

    # This is a combination of 7 commits. # This is the 1st commit message:
    add
    
    # The commit message #2 will be skipped:
    
    # 提交
    
    # The commit message #3 will be skipped:
    
    # add
    
    # The commit message #4 will be skipped:
    
    # add
    
    # The commit message #5 will be skipped:
    
    # add
    
    # The commit message #6 will be skipped:
    
    # add
    
    # The commit message #7 will be skipped:
    
    # add
    #
    # 完善
    #
    # 增加github 仓库地址
    #
    # Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # xinyu-alfred-open-in-vscode
 在Alfred中打开vscode项目
+
+## 使用方法
+首先需要在系统中安装vscode 的 code命令
+
+然后 npm i -g xinyu-alfred-open-in-vscode;
+
+然后在workfllows中配置需要打开的项目目录，参考：
+```
+要求是绝对的glob地址：/Users/aaa/work/*,/Users/aaa/home/*
+```
+![image](https://github.com/blank-x/xinyu-alfred-open-in-vscode/assets/16700540/e8e53143-6988-4d7b-a1b4-12ce4d4b5178)
+
+![image](https://github.com/blank-x/xinyu-alfred-open-in-vscode/assets/16700540/a2759b2c-824c-4dad-84d7-4d63b69c939d)
---
+++ index.js
@@ -0,0 +1,72 @@
+import alfy from "alfy";
+import path from "path";
+import glob from "fast-glob";
+const os = require('os');
+
+const wds = !!process.env?.alfred_vscode_wd ? process.env?.alfred_vscode_wd.split(",")	: [os.homedir()];
+
+const formatOutput = ({name, path}) => {
+	return {
+		title: name,
+		uid: path,
+		subtitle: path,
+		arg: path,
+		autocomplete: name,
+		"type": "file:skipcheck"
+	};
+}
+
+
+
+const projectCache = alfy.cache.get('projectCache');
+
+let parsedCache = projectCache ? JSON.parse(projectCache) : ['process.env.HOME'];
+if(parsedCache.length){
+	glob.async(wds, {
+		cwd: "/",
+		onlyDirectories: true,
+		dot: true,
+	}).then((d) => {
+		d = d || [];
+		d = d.map((item) => {
+			const name = path.basename(item);
+			return {
+				name,
+				path: item,
+			}
+		});
+		alfy.cache.set('projectCache', JSON.stringify(d));
+	})
+} else {
+	let d = glob.sync(wds, {
+		cwd: "/",
+		onlyDirectories: true,
+		dot: true,
+	}) || []
+	parsedCache = d.map((item) => {
+		return {
+			name: path.basename(item),
+			path: item,
+		}
+	});
+	alfy.cache.set('projectCache', JSON.stringify(parsedCache));
+}
+
+const input = alfy.input || '';
+
+const filtered = parsedCache
+										.filter(item=>item.name.includes(input))
+										.map((item=>{
+
+											return {
+												...item,
+												 index: item.name.indexOf(input)
+												}
+										}))
+										.sort((a, b)=>{
+											return b.index - a.index
+										}).map(formatOutput);
+ 
+alfy.output(filtered);
+
+
